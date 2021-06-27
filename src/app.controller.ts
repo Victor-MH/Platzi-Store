76a1ee2db8fd98da50edd68ff802214001e132f3
Victor Muñoz <victormu497@gmail.com>
2021-06-26 20:28:45 -0500

    Clase 6 completada, uso de decoradores y controladores

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,20 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('nest')
+    newEndpoint() {
+        return 'Aprendiendo Nest';
+    }
+
+    @Get('/ruta/') //No afectan las diagonales
+    hello() {
+        return 'con /sas/';
+    }
 }
