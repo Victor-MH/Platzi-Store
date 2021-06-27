9e00fe3c40a044bc80a727e8f160677043f418ca
Victor Muñoz <victormu497@gmail.com>
2021-06-26 21:03:32 -0500

    Uso de queries en parametros y manejo de choque entre rutas estáticas y dinámicas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -20,9 +20,23 @@ export class AppController {
         return 'con /sas/';
     }
 
+    //Lo tomaría como un id para la ruta pruducts/:id
+    //Para que no choquen hay que definir las rutas estáticas
+    //antes de las rutas dinámicas
+    //En este caso primero products/filter y debajo products/:id
+    @Get('products/filter')
+    getProductFilter() {
+        return `Yo soy un filtro`;
+    }
+
     @Get('products/:id')
     getProduct(@Param() params: any) {
         return `product ${params.id}`;
+    }
+
+    @Get('productos/:productId') //Definir el nombre del atributo que vamos a recibir
+    getProduct2(@Param('productId') productId: string) {
+        return `product ${productId}`;
     }
 
     @Get('categories/:id/products/:productId')
@@ -30,8 +44,14 @@ export class AppController {
         return `Product ${productId} from category ${id}`;
     }
 
-    @Get('productos/:productId') //Definir el nombre del atributo que vamos a recibir
-    getProduct2(@Param('productId') productId: string) {
-        return `product ${productId}`;
+    @Get('products')
+    getProducts(
+        @Query('limit') limit = 100, //Ya infiere que es number
+        @Query('offset') offset = 0, //Ahora usan parametros por defecto
+        @Query('brand') brand: string,
+        //products?limit=5&offset=9&brand=addidas
+    ) {
+        // const { limit, offset } = params;
+        return `Product from brand with ${brand} limit=${limit}, offset=${offset}`;
     }
 }
