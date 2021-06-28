344c36f819bea0aaf6ae70a0cd59ead44819cb75
Victor Muñoz <victormu497@gmail.com>
2021-06-27 23:49:22 -0500

    Clase 21 completada, Evitando parámetros incorrectos o maliciosos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            // Mínimo el whitelist, como backend dev decides si alertar al cliente y no continuar con el request o no
+            whitelist: true, // Va a quitar del payload todos los atributos que no estén definidos en el dto (Solo quita)
+            forbidNonWhitelisted: true, // Le dice a la API que le están enviando un atributo no definido (Alerta)
+        }),
+    );
     await app.listen(3000);
 }
 bootstrap();
