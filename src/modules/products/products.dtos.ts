44224ecd2bb9da060bc6b4a182d97fe513c624e9
Victor Muñoz <victormu497@gmail.com>
2021-06-27 23:24:21 -0500

    Clase 20 completada, validación de tipos de datos al momento de enviar las request, es decir, tendrá validación en producción. Uso de partial types para ahorrar código

--- src/modules/products/products.dtos.ts
+++ src/modules/products/products.dtos.ts
@@ -1,3 +1,13 @@
+import {
+    IsString,
+    IsNumber,
+    IsUrl,
+    IsNotEmpty,
+    IsPositive,
+} from 'class-validator';
+// Agrega la opción de opcional al dto base
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
     //Sirve en tiempo de desarrollo, evita que el desarrollador
     //cometa errores al programar, por ejemplo, muestra un error si
@@ -5,18 +15,32 @@ export class CreateProductDto {
     //que es de solo lectura
 
     //En ejecución no realiza la validación como tal
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly description: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive({ message: 'Price should be positive' }) //Mensajes personalizados
     readonly price: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
     readonly stock: number;
+
+    @IsUrl()
+    @IsNotEmpty()
     readonly image: string;
 }
 
-export class UpdateProductDto {
-    //Cómo son opcionales agregamos el ?
-    readonly name?: string;
-    readonly description?: string;
-    readonly price?: number;
-    readonly stock?: number;
-    readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
+
+// export class UpdateProductDto {
+//Cómo son opcionales agregamos el ?
+// readonly name?: string;
+// }
